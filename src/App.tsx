47d6b2c0fa8cdb14c819892dc02e8413fcec152d
nipper_san <totemo.nipper.san@gmail.com>
2025-03-01 14:55:26 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
+import AppTokPlatform from './apptok-platform';
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-        <div>
-          <div className="text-xl font-medium text-black">apptok</div>
-          <p className="text-gray-500">アプリケーションが正常に起動しました！</p>
-        </div>
-      </div>
-    </div>
+    <AppTokPlatform />
   );
 };
 
